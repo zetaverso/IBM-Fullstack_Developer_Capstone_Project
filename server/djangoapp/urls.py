e380743d124b9b065dc49e3e23904b02e567536f
zetaverso <zetaverso@outlook.com>
2024-03-09 01:12:05 -0500

    login-logout-register

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,18 +1,21 @@
 # Uncomment the imports before you add the code
-# from django.urls import path
+from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-# from . import views
+from . import views
 
 app_name = 'djangoapp'
 urlpatterns = [
     # # path for registration
 
     # path for login
-    # path(route='login', view=views.login_user, name='login'),
+    path(route='login', view=views.login_user, name='login'),
+    
+    # path for logout
+    path(route='logout', view=views.logout_url, name='logout'),
 
     # path for dealer reviews view
-
+    path(route='registration', view=views.registration, name='registration'),
     # path for add a review view
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
